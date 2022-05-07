cc43f0e5d2889099b25b2de1abd1dd60179e8772
Adam Beleko <adambeleko@gmail.com>
2022-05-07 21:59:02 +0300

    init

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'firebomb'
+import 'firebomb/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
