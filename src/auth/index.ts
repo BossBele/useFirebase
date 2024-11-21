0e6ec1d597b671bb0fc637d3ce8bf2e233c00619
Adam Beleko <bossbele@larven.io>
2024-11-21 06:33:54 +0300

    Add new authentication features - src/auth/index.ts

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,2 +1,6 @@
+export { default as getAuth } from './getAuth';
+export { default as AuthProvider  } from './AuthProvider';
 export { default as signIn  } from './signIn';
 export { default as signOut } from './signOut';
+export * from './tenant';
+export { default as useAuth } from './useAuth';
