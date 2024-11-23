794a3ce12bb82880bbf3dc057a62d0ecb06b719d
Adam Beleko <bossbele@larven.io>
2024-11-23 20:50:52 +0300

    Update dependencies and refactor auth provider - src/auth/getEmailAuthCredential.ts

--- src/auth/getEmailAuthCredential.ts
+++ src/auth/getEmailAuthCredential.ts
@@ -1,7 +1,7 @@
 import { EmailAuthCredential, EmailAuthProvider } from "firebase/auth";
 import getAuth from "./getAuth";
 
-export function getEmailAuthCredential(password = ''): EmailAuthCredential {
+export default function getEmailAuthCredential(password = ''): EmailAuthCredential {
   // function for getting credential for reauthentication
   const auth = getAuth();
   return EmailAuthProvider.credential(auth.currentUser.email, password);
