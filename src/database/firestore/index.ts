4537deaf2dffd1c728ac7a66db553938a9160755
Adam Beleko <bossbele@larven.io>
2024-11-23 20:50:53 +0300

    Update dependencies and refactor auth provider - src/database/firestore/index.ts

--- src/database/firestore/index.ts
+++ src/database/firestore/index.ts
@@ -1,7 +1,9 @@
+export { default as generateQuery } from './generateQuery';
 export { default as getFirestore } from './getFirestore';
 export { default as FirestoreProvider  } from './Provider';
 export { default as getDocument } from './getDocument';
 export { default as getDocuments } from './getDocuments';
 export { default as useCollection } from './useCollection';
+export { default as useCount } from './useCount';
 export { default as useDocument } from './useDocument';
 export { default as useFirestore } from './useFirestore';
