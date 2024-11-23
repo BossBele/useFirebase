235a82a72e56fdc812582666d5168d0a0cd1edd5
Adam Beleko <bossbele@larven.io>
2024-11-23 20:50:52 +0300

    Update dependencies and refactor auth provider - src/auth/index.ts

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,5 +1,8 @@
 export { default as getAuth } from './getAuth';
+export { default as getCurrentUser } from './getCurrentUser';
+export { default as getEmailAuthCredential } from './getEmailAuthCredential';
 export { default as AuthProvider  } from './Provider';
+export { default as sendPasswordResetEmail } from './sendPasswordResetEmail';
 export { default as signIn  } from './signIn';
 export { default as signOut } from './signOut';
 export * from './tenant';
