35f2f9694c199fa60a26fa9dddeb5309ed62e4f5
Adam Beleko <bossbele@larven.io>
2024-11-23 20:50:53 +0300

    Update dependencies and refactor auth provider - src/index.js

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { AuthProvider, useAuth, signIn } from './auth/useAuth'
-import { FirestoreProvider, useFirestore } from './useFirestore'
-
-// expose Auth and Firestore providers and hooks
-export { AuthProvider, useAuth, FirestoreProvider, useFirestore }
