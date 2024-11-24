31e898209ed9057997766fb9cbcbdbdd083e0a6c
Adam Beleko <bossbele@larven.io>
2024-11-24 12:22:17 +0300

    Add new exports and update dependencies - src/database/firestore/getDocuments.ts

--- src/database/firestore/getDocuments.ts
+++ src/database/firestore/getDocuments.ts
@@ -2,7 +2,7 @@ import { getDocs } from 'firebase/firestore';
 import { GenericObject, IGetDocumentsOptions } from './types';
 import generateQuery from './generateQuery';
 
-export default async function getDocuments(collection: string, options: IGetDocumentsOptions): Promise<GenericObject[]> {
+export default async function getDocuments(collection: string, options?: IGetDocumentsOptions): Promise<GenericObject[]> {
   const query = generateQuery(collection, options);
 
   const querySnapshot = await getDocs(query);
