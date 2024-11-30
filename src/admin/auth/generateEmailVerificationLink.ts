a5f136955e7348341485e62b3bf4f49761c94068
Adam Beleko <bossbele@larven.io>
2024-11-30 12:07:16 +0300

    Add admin nodejs module - src/admin/auth/generateEmailVerificationLink.ts

---
+++ src/admin/auth/generateEmailVerificationLink.ts
@@ -0,0 +1,16 @@
+import { ActionCodeSettings } from "firebase-admin/auth";
+import getAuth from "./getAuth";
+
+/**
+ * @function generateEmailVerificationLink
+ * Admin SDK API to generate the email verification link.
+ * @param {string} email - The email address to verify
+ * @param {ActionCodeSettings} [actionCodeSettings]
+ * @returns {Promise<string>}
+ */
+export default async function generateEmailVerificationLink(email, actionCodeSettings?: ActionCodeSettings): Promise<string> {
+  const auth = getAuth();
+  // Construct email verification template, embed the link and send using custom SMTP server.
+  const link = await auth.generateEmailVerificationLink(email, actionCodeSettings);
+  return link;
+}
