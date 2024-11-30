e31a19f7944665189c344c7edfd1c72834bda059
Adam Beleko <bossbele@larven.io>
2024-11-30 08:44:53 +0300

    Add admin nodejs module - src/auth/signOut.ts

--- src/auth/signOut.ts
+++
@@ -1,7 +0,0 @@
-import { signOut as signOutFB } from 'firebase/auth';
-import getAuth from "./getAuth";
-
-export default async function signOut(): Promise<void> {
-  const auth = getAuth();
-  return signOutFB(auth);
-}
