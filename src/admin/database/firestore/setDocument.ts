0cd415c513fe29bd803efdbd07263ee94218ac71
Adam Beleko <bossbele@larven.io>
2024-11-30 12:07:16 +0300

    Add admin nodejs module - src/admin/database/firestore/setDocument.ts

---
+++ src/admin/database/firestore/setDocument.ts
@@ -0,0 +1,28 @@
+import { GenericObject } from "../../../web/database/firestore/types";
+import getFirestore from "./getFirestore";
+
+export async function setDocument(collection: string, form: GenericObject): Promise<string>;
+export async function setDocument(collection: string, documentId: string, form: GenericObject): Promise<string>;
+
+/**
+ * @function setDocument
+ * Set a document in a collection
+ * @param {string} collection - The collection name
+ * @param {string|GenericObject} arg2 - The documentId or form object; if form is provided, a document with random id be created
+ * @param {GenericObject} [form] - The form to set/update if documentId is provided
+ * @returns {Promise<string>} - The documentId
+ */
+export default async function setDocument(collection: string, arg2: string|GenericObject, form?: GenericObject): Promise<string> {
+  const firestore = getFirestore();
+  const collectionRef = firestore.collection(collection);
+
+  let refId = typeof arg2 === 'string' ? arg2 : '';
+  if (typeof arg2 === 'string') {
+    // documentId provided
+    await collectionRef.doc(arg2).set(form, { merge: true });
+  } else {
+    const docRef = await collectionRef.add(form);
+    refId = docRef.id;
+  }
+  return refId;
+}
