60116062cfc735f1b48a66a8a5682133be6d7e62
Adam Beleko <bossbele@larven.io>
2024-11-30 08:44:54 +0300

    Add admin nodejs module - src/getApp.ts

--- src/getApp.ts
+++
@@ -1,9 +0,0 @@
-import { FirebaseApp, getApp as getAppFB, getApps } from 'firebase/app';
-
-export default function getApp(): FirebaseApp|null {
-  if(getApps()?.length) {
-    const app = getAppFB();
-    return app;
-  }
-  return null;
-}
