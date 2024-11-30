230ae7faac4b207c92ddb0e251c5d179bd57513e
Adam Beleko <bossbele@larven.io>
2024-11-30 08:44:53 +0300

    Add admin nodejs module - src/auth/useAuth.ts

--- src/auth/useAuth.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { AuthContext } from './Provider';
-import { UserState } from './types';
-
-const useAuth = (): UserState => useContext(AuthContext);
-export default useAuth;
