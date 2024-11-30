338ed88c894c43b24fe09742c286262b548b63f9
Adam Beleko <bossbele@larven.io>
2024-11-30 08:44:53 +0300

    Add admin nodejs module - src/auth/sendPasswordResetEmail.ts

--- src/auth/sendPasswordResetEmail.ts
+++
@@ -1,7 +0,0 @@
-import { sendPasswordResetEmail as sendPasswordResetEmailController } from "firebase/auth";
-import getAuth from "./getAuth";
-
-export default async function sendPasswordResetEmail(email = '', url = ''): Promise<void> {
-  const auth = getAuth();
-  await sendPasswordResetEmailController(auth, email, { url });
-}
