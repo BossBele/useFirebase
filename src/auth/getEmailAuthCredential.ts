61b56c293c7347a3d36a4708863c7d0dc6b89b85
Adam Beleko <bossbele@larven.io>
2024-11-30 08:44:53 +0300

    Add admin nodejs module - src/auth/getEmailAuthCredential.ts

--- src/auth/getEmailAuthCredential.ts
+++
@@ -1,8 +0,0 @@
-import { EmailAuthCredential, EmailAuthProvider } from "firebase/auth";
-import getAuth from "./getAuth";
-
-export default function getEmailAuthCredential(password = ''): EmailAuthCredential {
-  // function for getting credential for reauthentication
-  const auth = getAuth();
-  return EmailAuthProvider.credential(auth.currentUser.email, password);
-}
