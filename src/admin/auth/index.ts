a5cd42739624b2ab553e6df6eb61708ae544d4c1
Adam Beleko <bossbele@larven.io>
2024-11-30 12:07:16 +0300

    Add admin nodejs module - src/admin/auth/index.ts

--- src/admin/auth/index.ts
+++ src/admin/auth/index.ts
@@ -3,3 +3,7 @@ import admin from "../";
 const auth = admin.auth();
 
 export default auth;
+
+export { default as getAuth } from "./getAuth";
+export { default as generateEmailVerificationLink } from "./generateEmailVerificationLink";
+export { default as generatePasswordResetLink } from "./generatePasswordResetLink";
