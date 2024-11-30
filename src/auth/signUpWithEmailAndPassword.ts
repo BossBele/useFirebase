a76aabb73fd436ce55f58c15abff921b3ef83eaf
Adam Beleko <bossbele@larven.io>
2024-11-30 08:44:53 +0300

    Add admin nodejs module - src/auth/signUpWithEmailAndPassword.ts

--- src/auth/signUpWithEmailAndPassword.ts
+++
@@ -1,8 +0,0 @@
-import { UserCredential, createUserWithEmailAndPassword } from "firebase/auth";
-import getAuth from "./getAuth";
-
-export default async function signUpWithEmailAndPassword(email = '', password = ''): Promise<UserCredential> {
-  const auth = getAuth();
-  const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-  return userCredential;
-}
