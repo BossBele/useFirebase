b760521ec50c79e1238d82095a3baff29a35dedf
Adam Beleko <bossbele@larven.io>
2024-11-30 08:44:54 +0300

    Add admin nodejs module - src/database/firestore/getFirestore.ts

--- src/database/firestore/getFirestore.ts
+++
@@ -1,9 +0,0 @@
-import { Firestore, getFirestore as getFirestoreFB } from 'firebase/firestore';
-import getApp from '../../getApp';
-
-export default function getFirestore(): Firestore|null {
-    if (getApp()) {
-      return getFirestoreFB();
-    }
-    return null;
-}
