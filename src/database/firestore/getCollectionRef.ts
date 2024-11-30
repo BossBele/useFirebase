adaa4bc3e4adf37af75a7e31a0fb4d638ae24b0d
Adam Beleko <bossbele@larven.io>
2024-11-30 08:44:54 +0300

    Add admin nodejs module - src/database/firestore/getCollectionRef.ts

--- src/database/firestore/getCollectionRef.ts
+++
@@ -1,9 +0,0 @@
-import { DocumentData, CollectionReference, collection as collectionFB } from 'firebase/firestore';
-import getFirestore from './getFirestore';
-
-export default function getCollectionRef(collection: string): CollectionReference<DocumentData, DocumentData> {
-  const firestore = getFirestore();
-
-  const collecionRef = collectionFB(firestore, collection);
-  return collecionRef;
-}
