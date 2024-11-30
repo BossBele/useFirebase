67cfc20289d991e3ba68529d055e752444b70721
Adam Beleko <bossbele@larven.io>
2024-11-30 08:44:53 +0300

    Add admin nodejs module - src/auth/getCurrentUser.ts

--- src/auth/getCurrentUser.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "firebase/auth";
-import getAuth from "./getAuth";
-
-export default function getCurrentUser(): User | null {
-  const auth = getAuth();
-  return auth.currentUser;
-}
