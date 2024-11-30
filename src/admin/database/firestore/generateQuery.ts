2b2db1178d280204cc84567f9646c7fce77f1150
Adam Beleko <bossbele@larven.io>
2024-11-30 12:07:16 +0300

    Add admin nodejs module - src/admin/database/firestore/generateQuery.ts

---
+++ src/admin/database/firestore/generateQuery.ts
@@ -0,0 +1,33 @@
+import { DocumentData, Query } from "firebase-admin/firestore";
+import getFirestore from "./getFirestore";
+import { IDocumentsQueryOptions } from "./types";
+
+export default function generateQuery(collection: string, options: IDocumentsQueryOptions): Query<DocumentData, DocumentData> {
+  const firestore = getFirestore();
+
+  const store = firestore.collection(collection);
+
+  let queries: Query<DocumentData, DocumentData>;
+  if (options?.whereBy?.length) {
+    queries = options.whereBy.reduce((prev, {
+      field, operator, value,
+    }) => prev.where(field, operator, value), store);
+  }
+
+  if (options?.orderBy?.length) {
+    queries = options.orderBy.reduce(
+      (prev, { field, direction }) => prev.orderBy(field, direction),
+      queries,
+    );
+  }
+
+  if (options?.limit) {
+    queries = queries.limit(options.limit);
+  }
+
+  if (options?.select) {
+    queries = queries.select(...options.select);
+  }
+
+  return queries;
+}
