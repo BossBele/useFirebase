d1e7a602dccefea0cd5725eec702468790f6c988
Adam Beleko <bossbele@larven.io>
2024-11-30 08:44:54 +0300

    Add admin nodejs module - src/database/firestore/useFirestore.ts

--- src/database/firestore/useFirestore.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { FirestoreContext } from './Provider';
-import { IUseFirestore } from './types';
-
-const useFirestore = (): IUseFirestore => useContext(FirestoreContext);
-export default useFirestore;
