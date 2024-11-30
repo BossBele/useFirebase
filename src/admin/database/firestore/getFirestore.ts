29fe89230d861e4515f4506462db8d730e53e722
Adam Beleko <bossbele@larven.io>
2024-11-30 12:07:16 +0300

    Add admin nodejs module - src/admin/database/firestore/getFirestore.ts

--- src/admin/database/firestore/getFirestore.ts
+++ src/admin/database/firestore/getFirestore.ts
@@ -1,8 +1,16 @@
 import { App } from "firebase-admin/app";
 import { Firestore, getFirestore as getFirestoreFB } from "firebase-admin/firestore";
 
-export function getFirestore(databaseId: string): Firestore;
-export function getFirestore(app: App): Firestore;
-export default function getFirestore(app): Firestore {
+export function getFirestore(): Firestore;
+export function getFirestore(databaseId?: string): Firestore;
+export function getFirestore(app?: App): Firestore;
+
+/**
+ * @function getFirestore
+ * Get a Firestore service for the default app or a given app
+ * @param {App|string} [app]
+ * @returns {Firestore}
+ */
+export default function getFirestore(app?: any): Firestore {
   return getFirestoreFB(app);
 }
