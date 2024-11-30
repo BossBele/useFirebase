8e7822f2c160df10539929978e470308c61d2561
Adam Beleko <bossbele@larven.io>
2024-11-30 08:44:53 +0300

    Add admin nodejs module - src/auth/index.ts

--- src/auth/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as getAuth } from './getAuth';
-export { default as getCurrentUser } from './getCurrentUser';
-export { default as getEmailAuthCredential } from './getEmailAuthCredential';
-export { default as AuthProvider  } from './Provider';
-export { default as sendPasswordResetEmail } from './sendPasswordResetEmail';
-export { default as signIn  } from './signIn';
-export { default as signOut } from './signOut';
-export * from './tenant';
-export { default as useAuth } from './useAuth';
