1a2f25dbbbc96163ecf4bfec30be68f60aa9c787
Adam Beleko <bossbele@larven.io>
2024-12-08 00:38:14 +0300

    Export admin and web modules - src/web/database/firestore/types.ts

--- src/web/database/firestore/types.ts
+++ src/web/database/firestore/types.ts
@@ -1,4 +1,4 @@
-import { OrderByDirection, WhereFilterOp } from "firebase/firestore";
+import { FieldValue, OrderByDirection, Timestamp, WhereFilterOp } from "firebase/firestore";
 
 export type PersistenceType = 'context' | 'local' | 'session' | 'none';
 export enum PersistenceTypes {
@@ -14,6 +14,25 @@ export interface IUseCollection {
   enabled: boolean,
   persistence?: PersistenceType,
   withRealtimeUpdates: boolean,
+}
+
+export interface IUseCollectionValue {
+  isFetching: boolean,
+  isFetched: boolean,
+  records: GenericObject[],
+}
+
+export interface IUseDocument {
+  collection: string,
+  documentId: string,
+  enabled?: boolean,
+  withRealtimeUpdates?: boolean,
+}
+
+export interface IUseDocumentValue {
+  isFetching: boolean,
+  isFetched: boolean,
+  record: GenericObject|null,
 }
 
 export type GenericObject = { [key: string]: any };
@@ -53,5 +72,83 @@ export interface IUseCount {
   enabled: boolean,
 }
 
+export interface IUseCountValue {
+  count: number,
+  isCounted: boolean,
+  isEmpty: boolean,
+}
+
 export type setDocument = (collection: string, documentId: string, form: GenericObject) => Promise<string>;
 export type addDocument = (collection: string, form: GenericObject) => Promise<string>;
+
+export interface ICollectionModel {
+  getName(): string;
+  getSchema(): GenericObject;
+  getConstraints(): IGetDocumentsOptions;
+  withConstraints(constraints: IGetDocumentsOptions): this;
+  whereBy(constraint: IGetDocumentsOptions['whereBy']): this;
+  whereOr(constraint: IGetDocumentsOptions['whereOr']): this;
+  orderBy(constraint: IGetDocumentsOptions['orderBy']): this;
+  limit(constraint: IGetDocumentsOptions['limit']): this;
+  getCount(): Promise<number>;
+  getDocuments(): Promise<GenericObject[]>;
+}
+
+// Base document interface that all Firestore docs should extend
+export interface BaseDocument {
+  id: string;
+  createdAt?: Timestamp;
+  updatedAt?: Timestamp;
+}
+
+export interface IDocumentModel<T extends BaseDocument> {
+  /**
+   * Gets the collection name of the document
+   */
+  getCollection(): string;
+  /**
+   * Gets the id of the document
+   */
+  getId(): string;
+  /**
+   * Sets the id for the document
+   * @param value - The id to set for the document
+   */
+  setId(value: string): void;
+  /**
+   * Gets the changes made to the document as JSON object
+   */
+  getChanges(): Partial<T>;
+  /**
+   * Resets the changes made to the document
+   */
+  resetChanges(): void;
+  /**
+   * Checks if any of the document fields have changed
+   */
+  hasChanged(): boolean;
+  /**
+   * Checks if the specified fields have changed
+   * @param fields - List of fields to check if they have changed
+   */
+  hasFieldsChanged(...fields: string[]): boolean;
+  /**
+   * Commits the changes to the Firestore document
+   * Adds a new document if the document does not exist, updates if exists
+   * If id was not set, a document with random firestore Id be created
+   * @returns - The updated document data or false if no changes were made
+   */
+  commit(): Promise<GenericObject|false>;
+  /**
+   * Converts the document data to a JSON object
+   */
+  toJSON(): GenericObject;
+  /**
+   * Converts the document data to a JSON string
+   */
+  toString(): string;
+}
+
+export type DocumentModelInstance<T extends BaseDocument> = {
+  [key: string]: FieldValue;
+} & IDocumentModel<T>;
