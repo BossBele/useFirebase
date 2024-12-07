10890f36730a5b13656418a2566487047ce8680e
Adam Beleko <bossbele@larven.io>
2024-12-08 00:38:14 +0300

    Export admin and web modules - src/web/auth/getEmailAuthCredential.ts

--- src/web/auth/getEmailAuthCredential.ts
+++ src/web/auth/getEmailAuthCredential.ts
@@ -1,7 +1,7 @@
 import { EmailAuthCredential, EmailAuthProvider } from "firebase/auth";
 import getAuth from "./getAuth";
 
-export default function getEmailAuthCredential(password = ''): EmailAuthCredential {
+export default function getEmailAuthCredential(password: string): EmailAuthCredential {
   // function for getting credential for reauthentication
   const auth = getAuth();
   return EmailAuthProvider.credential(auth.currentUser.email, password);
