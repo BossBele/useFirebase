8bd9fc42b5f1e78b9ca17bc6c77641b5a6087222
Adam Beleko <bossbele@larven.io>
2024-12-08 00:38:14 +0300

    Export admin and web modules - src/web/auth/sendPasswordResetEmail.ts

--- src/web/auth/sendPasswordResetEmail.ts
+++ src/web/auth/sendPasswordResetEmail.ts
@@ -1,7 +1,7 @@
 import { sendPasswordResetEmail as sendPasswordResetEmailController } from "firebase/auth";
 import getAuth from "./getAuth";
 
-export default async function sendPasswordResetEmail(email = '', url = ''): Promise<void> {
+export default async function sendPasswordResetEmail(email: string, url: string): Promise<void> {
   const auth = getAuth();
   await sendPasswordResetEmailController(auth, email, { url });
 }
