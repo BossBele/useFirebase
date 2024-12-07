bacc28181cd256a9902d2da8e00a1b1fc625836e
Adam Beleko <bossbele@larven.io>
2024-12-08 00:38:14 +0300

    Export admin and web modules - src/admin/types.ts

---
+++ src/admin/types.ts
@@ -0,0 +1,30 @@
+import type { App } from 'firebase-admin/app';
+import type { ActionCodeSettings, Auth } from 'firebase-admin/auth';
+import type { CollectionReference, DocumentData, DocumentReference, Firestore, Query } from 'firebase-admin/firestore';
+import type { Messaging } from 'firebase-admin/messaging';
+import type { IDocumentsQueryOptions } from './database/firestore/types';
+import type { GenericObject, IGetDocumentsOptions } from '../web/database/firestore/types';
+
+export interface AdminModule {
+  admin: object,
+  auth: Auth,
+  generateEmailVerificationLink: (email: string, actionCodeSettings?: ActionCodeSettings) => Promise<string>,
+  generatePasswordResetLink: (email: string, actionCodeSettings?: ActionCodeSettings) => Promise<string>,
+  getAuth: (app?: App) => Auth,
+  firestore: Firestore,
+  getFirestore: (app?: App) => Firestore,
+  deleteDocument: (collection: string, document: string) => Promise<FirebaseFirestore.WriteResult>
+  generateQuery: (collection: string, options?: IDocumentsQueryOptions) => Query<DocumentData, DocumentData>,
+  getCollectionRef: (collection: string) => CollectionReference,
+  getDocument: (collection: string, document: string) => Promise<GenericObject|null>,
+  getDocumentBy: (collection: string, options: Omit<IDocumentsQueryOptions, 'limit'>) => Promise<GenericObject|null>,
+  getDocumentIds: (collection: string) => Promise<string[]>,
+  getDocumentRef: (collection: string, document: string) => DocumentReference,
+  getDocuments: (collection: string, options?: IDocumentsQueryOptions) => Promise<GenericObject[]>,
+  getDocumentsCount: (collection: string, options?: IDocumentsQueryOptions) => Promise<number>,
+  getFieldSummation: (collection: string, aggregateField: string, options?: IGetDocumentsOptions) => Promise<number>,
+  listCollections: (databaseId?: string) => Promise<string[]>,
+  setDocument: (collection: string, arg2: string|GenericObject, form?: GenericObject) => Promise<string>,
+  messaging: Messaging,
+  getMessaging: (app?: App) => Messaging,
+}
