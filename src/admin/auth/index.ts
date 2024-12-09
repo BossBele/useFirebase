b47c3e66913d5e727fca15bce2fabbf538d74fdf
Adam Beleko <bossbele@larven.io>
2024-12-09 16:05:33 +0300

    Add session cookie management - src/admin/auth/index.ts

--- src/admin/auth/index.ts
+++ src/admin/auth/index.ts
@@ -5,5 +5,10 @@ const auth = admin.auth();
 export default auth;
 
 export { default as getAuth } from "./getAuth";
+export { default as createSessionCookie } from "./createSessionCookie";
 export { default as generateEmailVerificationLink } from "./generateEmailVerificationLink";
 export { default as generatePasswordResetLink } from "./generatePasswordResetLink";
+export { default as getCurrentUser } from "./getCurrentUser";
+export { default as getSession } from "./getSession";
+export { default as isUserAuthenticated } from "./isUserAuthenticated";
+export { default as revokeSession } from "./revokeSession";
