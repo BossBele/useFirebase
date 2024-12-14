f29bc6283dfda016cea7f2b2ccfedc21158182b3
Adam Beleko <bossbele@larven.io>
2024-12-14 15:57:46 +0300

    Add firebase messaging - src/admin/auth/isUserAuthenticated.ts

--- src/admin/auth/isUserAuthenticated.ts
+++ src/admin/auth/isUserAuthenticated.ts
@@ -1,13 +1,22 @@
 import getAuth from './getAuth';
 import getSession from './getSession';
+import type { IAuthOptions } from './types';
 
 export default async function isUserAuthenticated(
-  session?: string
+  session?: string,
+  options?: IAuthOptions,
 ): Promise<boolean> {
   const _session = session ?? (await getSession());
   if (!_session) return false;
 
-  const auth = getAuth();
+  const authOptions: IAuthOptions = {};
+  if (options?.tenantId) {
+    authOptions.tenantId = options.tenantId;
+  }
+  if (options?.app) {
+    authOptions.app = options.app;
+  }
+  const auth = getAuth(authOptions);
   try {
     const decodedIdToken = await auth.verifySessionCookie(_session, true);
     return Boolean(decodedIdToken?.uid);
