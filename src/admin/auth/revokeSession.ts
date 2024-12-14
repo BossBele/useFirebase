7e804d2b41f7aa4258cb1d7f8968e5fb2bb8084f
Adam Beleko <bossbele@larven.io>
2024-12-14 13:51:01 +0300

    Add firebase messaging - src/admin/auth/revokeSession.ts

--- src/admin/auth/revokeSession.ts
+++ src/admin/auth/revokeSession.ts
@@ -5,7 +5,7 @@ export default async function revokeSession(session: string): Promise<void> {
   const auth = getAuth();
   const decodedIdToken = await auth.verifySessionCookie(session);
   // to unset a cookie, use a date in the past, ex: { expires: new Date(0) }
-  cookies.set('__session', '', { expires: new Date(0) });
+  cookies().set('__session', '', { expires: new Date(0) });
 
   return await auth.revokeRefreshTokens(decodedIdToken.sub);
 }
