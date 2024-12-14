7f99a6c8de27ed8bb90b81e88f8588bf74751526
Adam Beleko <bossbele@larven.io>
2024-12-14 15:57:46 +0300

    Add firebase messaging - src/admin/auth/getAuth.ts

--- src/admin/auth/getAuth.ts
+++ src/admin/auth/getAuth.ts
@@ -1,6 +1,23 @@
 import type { App } from "firebase-admin/app";
-import { Auth, getAuth as getAuthFB } from "firebase-admin/auth";
+import { Auth, TenantAwareAuth, getAuth as getAuthFB } from "firebase-admin/auth";
+import type { IAuthOptions } from "./types";
 
-export default function getAuth(app?: App): Auth {
-  return getAuthFB(app);
+export function getAuth(app?: App): Auth;
+export function getAuth(authOptions?: IAuthOptions): TenantAwareAuth;
+
+/**
+ * @function getAuth
+ * Get the Auth instance for the default app or a given app.
+ * @param {App|IAuthOptions} [param] - The app or options to use.
+ * @returns {Auth|TenantAwareAuth} - Auth instance
+ */
+export default function getAuth(param?: App|IAuthOptions): Auth|TenantAwareAuth {
+  const authOptions = param as IAuthOptions;
+  const appParam = param as App;
+  const app = appParam?.name ? appParam : authOptions?.app;
+  let auth: Auth|TenantAwareAuth = getAuthFB(app);
+  if (authOptions?.tenantId) {
+    auth = auth.tenantManager().authForTenant(authOptions.tenantId);
+  }
+  return auth;
 }
