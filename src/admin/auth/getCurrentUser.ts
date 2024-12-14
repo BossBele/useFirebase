ad8456789f41736a2e5deebf7ddfb2739bc53ca5
Adam Beleko <bossbele@larven.io>
2024-12-14 15:57:46 +0300

    Add firebase messaging - src/admin/auth/getCurrentUser.ts

--- src/admin/auth/getCurrentUser.ts
+++ src/admin/auth/getCurrentUser.ts
@@ -2,12 +2,21 @@ import type { UserRecord } from 'firebase-admin/auth';
 import getAuth from './getAuth';
 import isUserAuthenticated from './isUserAuthenticated';
 import getSession from './getSession';
+import type { IAuthOptions } from './types';
 
-export default async function getCurrentUser(): Promise<UserRecord> {
-  const auth = getAuth();
+export default async function getCurrentUser(options: IAuthOptions): Promise<UserRecord> {
+  const authOptions: IAuthOptions = {};
+  if (options?.tenantId) {
+    authOptions.tenantId = options.tenantId;
+  }
+  if (options?.app) {
+    authOptions.app = options.app;
+  }
+
+  const auth = getAuth(authOptions);
   const session = await getSession();
 
-  if (!(await isUserAuthenticated(session))) {
+  if (!(await isUserAuthenticated(session, authOptions))) {
     return null;
   }
 
