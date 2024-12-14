1635e9cee2f20d3d69358e9c7be89b626a4a517b
Adam Beleko <bossbele@larven.io>
2024-12-14 15:57:46 +0300

    Add firebase messaging - src/admin/auth/createSessionCookie.ts

--- src/admin/auth/createSessionCookie.ts
+++ src/admin/auth/createSessionCookie.ts
@@ -1,13 +1,24 @@
 import cookies from '@boiseitguru/cookie-cutter';
 import type { SessionCookieOptions } from 'firebase-admin/auth';
 import getAuth from './getAuth';
+import { IAuthOptions } from './types';
 
 export default async function createSessionCookie(
   idToken: string,
-  sessionCookieOptions: SessionCookieOptions
+  sessionCookieOptions: SessionCookieOptions & IAuthOptions,
 ): Promise<string> {
-  const auth = getAuth();
-  const session = await auth.createSessionCookie(idToken, sessionCookieOptions);
-  cookies().set('__session', session, { secure: true, ...sessionCookieOptions });
+  const { tenantId, app, ...cookieOptions } = sessionCookieOptions;
+
+  const authOptions: IAuthOptions = {};
+  if (tenantId) {
+    authOptions.tenantId = tenantId;
+  }
+  if (app) {
+    authOptions.app = app;
+  }
+
+  const auth = getAuth(authOptions);
+  const session = await auth.createSessionCookie(idToken, cookieOptions);
+  cookies().set('__session', session, { secure: true, ...cookieOptions });
   return session;
 }
