db855bdc042c1a999d4f412e224509ffa6dd0f6b
Adam Beleko <bossbele@larven.io>
2024-12-22 10:06:04 +0300

    fixes

--- src/admin/database/firestore/types.ts
+++ src/admin/database/firestore/types.ts
@@ -1,6 +1,14 @@
-import { IGetDocumentsOptions } from "../../../web/database/firestore/types";
+import { ICollectionModel, IGetDocumentsOptions } from "../../../web/database/firestore/types";
 
 export interface IDocumentsQueryOptions extends IGetDocumentsOptions {
   select?: string[],
   withSubcollections?: boolean,
+}
+
+export interface IAdminCollectionModel<T> extends ICollectionModel<T> {
+  select(constraint: IDocumentsQueryOptions['select']): this,
+  /**
+   * @description Enables fetching subcollections of the documents
+   */
+  withSubcollections(): this,
 }
