642a1a71135e5ef300d1d2b42c3338339ce531a1
Adam Beleko <bossbele@larven.io>
2024-12-26 11:56:34 +0300

    expose more db function - src/web/database/firestore/getDocumentRef.ts

--- src/web/database/firestore/getDocumentRef.ts
+++ src/web/database/firestore/getDocumentRef.ts
@@ -1,8 +1,8 @@
 import { DocumentData, DocumentReference, doc } from 'firebase/firestore';
 import getFirestore from './getFirestore';
 
-export default function getDocumentRef(collection: string, documentId: string): DocumentReference<DocumentData, DocumentData> {
-  const firestore = getFirestore();
+export default function getDocumentRef(collection: string, documentId: string, databaseId?: string): DocumentReference<DocumentData, DocumentData> {
+  const firestore = getFirestore(databaseId);
 
   const docRef = doc(firestore, collection, documentId);
   return docRef;
