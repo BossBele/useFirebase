aad7db9dd48375049b3f5eed6490275e1a4f4539
Adam Beleko <bossbele@larven.io>
2024-12-27 13:09:15 +0300

    fix usecollection - src/web/database/firestore/useCollection.ts

--- src/web/database/firestore/useCollection.ts
+++ src/web/database/firestore/useCollection.ts
@@ -2,16 +2,14 @@ import {
   useCallback,
   useEffect,
   useMemo,
-  useRef,
   useState
 } from 'react';
 import { getApps } from 'firebase/app';
-import { DocumentData, getDocs, onSnapshot, query as queryFn, Query, QueryDocumentSnapshot, startAfter, queryEqual } from 'firebase/firestore';
+import { getDocs, onSnapshot, Query } from 'firebase/firestore';
 import { IUseCollection, IUseCollectionValue } from './types';
 import useFirestore from './useFirestore';
 import { checkPersistenceSupport, getSessionItem, getStorage, setSessionItem, setStorage } from '../../utils';
 import generateQuery from './generateQuery';
-import useEffectDebugger from '../../useEffectDebugger';
 
 export default function useCollection({
     collection,
@@ -19,22 +17,15 @@ export default function useCollection({
     persistence = 'context',
     withRealtimeUpdates = true,
     constraints: constraintsProp,
-    page = 1,
 }: IUseCollection): IUseCollectionValue {
   const { retrieveItem, storeItem } = useFirestore();
-
-  const currentQuery = useRef<Query|null>(null);
-  const lastVisibleDoc = useRef<{[key: number]: QueryDocumentSnapshot<DocumentData, DocumentData>}>({});
 
   const [isFetching, setIsFetching] = useState(false);
   const [records, setRecords] = useState(null);
   
-  const constraints = useMemo(() => ({
-    ...constraintsProp,
-    ...(page > 1 ? { limit: page * (constraintsProp?.limit * 10) } : {}),
-  }), [constraintsProp, page]);
+  const constraints = useMemo(() => constraintsProp, [constraintsProp]);
 
-  const cachedData = useMemo(() => {
+  const getCachedData = useCallback(() => {
     switch (persistence) {
         case 'context':
             return retrieveItem(collection);
@@ -47,18 +38,15 @@ export default function useCollection({
             break;
     }
     return null;
-  }, [retrieveItem, collection]);
+  }, [collection, persistence, retrieveItem]);
 
   const isEnabled: boolean = useMemo(() => {
     return Boolean(collection) && enabled
   }, [collection, enabled]);
 
   const query: Query = useMemo(() => {
-    if (page > 1 && lastVisibleDoc.current?.[page]) {
-      return queryFn(generateQuery(collection, constraints), startAfter(lastVisibleDoc.current));
-    }
     return generateQuery(collection, constraints);
-  }, [collection, constraints, page]);
+  }, [collection, constraints]);
 
   const persistCache = useCallback((value) => {
     switch (persistence) {
@@ -78,74 +66,30 @@ export default function useCollection({
     persistCache(results);
   }, [persistCache]);
 
-  /**
-   * @function getCollection - subscribe to firestore query/collection
-   * @param {import("firebase/firestore").Query} query - Firestore query
-   * @param {string} collection - Query name or collection name
-   * @returns {(import("firebase/firestore").Unsubscribe|function())} - Unsubscribe function
-   */
-  // const getCollection = useCallback(async () => {
-  //   if (!isEnabled) {
-  //     return;
-  //   }
-  //   // check if firebase is initialized
-  //   if (getApps().length < 1) {
-  //     throw new Error('Firebase is not initialized');
-  //   }
-
-  //   setIsFetching(true);
-  //   let unsubscribe = () => {};
-
-  //   const finalQuery = lastVisibleDoc.current ? queryFn(query, startAfter(lastVisibleDoc.current)): query;
-
-  //   const results: object[] = [];
-  //   if (withRealtimeUpdates) {
-  //     // listen for firestore changes
-  //     unsubscribe = onSnapshot(finalQuery, (querySnapshot) => {
-  //       querySnapshot.docs.forEach((doc) => {
-  //         const object = doc.data();
-  //         results.push({ ...object, id: doc.id });
-  //       });
-  //       if (withPagination && querySnapshot.docs.length) {
-  //         // Get the last visible document
-  //         lastVisibleDoc.current = querySnapshot.docs[querySnapshot.docs.length - 1];
-  //       }
-  //     });
-  //   } else {
-  //     const querySnapshot = await getDocs(finalQuery);
-  //     querySnapshot.forEach((doc) => {
-  //         results.push({ ...doc.data(), id: doc.id });
-  //     });
-  //     if (withPagination && querySnapshot.docs.length) {
-  //       // Get the last visible document
-  //       lastVisibleDoc.current = querySnapshot.docs[querySnapshot.docs.length - 1];
-  //     }
-  //   }
-
-  //   handleOnResults(results);
-  //   setIsFetching(false);
-  //   return unsubscribe;
-  // }, [query, isEnabled, handleOnResults, withPagination, withRealtimeUpdates]);
-
   useEffect(() => {
     checkPersistenceSupport(persistence);
   }, [persistence]);
 
-  useEffectDebugger(() => {
+  useEffect(() => {
+    /**
+     * @function getCollection - subscribe to firestore query/collection
+     * @returns {Promise<import("firebase/firestore").Unsubscribe|function()>} - Unsubscribe function
+     */
     async function getCollection() {
-      if (!isEnabled || queryEqual(query, currentQuery.current)) {
+      if (!isEnabled) {
         return;
+      }
+      if (getCachedData()?.length >= constraints?.limit) {
+        setRecords(getCachedData());
       }
       // check if firebase is initialized
       if (getApps().length < 1) {
         throw new Error('Firebase is not initialized');
       }
-  
+
       setIsFetching(true);
       let unsubscribe = () => {};
-  
-      currentQuery.current = query;
-  
+
       const results: object[] = [];
       if (withRealtimeUpdates) {
         // listen for firestore changes
@@ -154,28 +98,21 @@ export default function useCollection({
             const object = doc.data();
             results.push({ ...object, id: doc.id });
           });
-          if (querySnapshot.docs.length) {
-            // Get the last visible document
-            lastVisibleDoc.current[page] = querySnapshot.docs[querySnapshot.docs.length - 1];
-          }
+          handleOnResults(results);
         });
       } else {
         const querySnapshot = await getDocs(query);
         querySnapshot.forEach((doc) => {
             results.push({ ...doc.data(), id: doc.id });
         });
-        if (querySnapshot.docs.length) {
-          // Get the last visible document
-          lastVisibleDoc.current[page] = querySnapshot.docs[querySnapshot.docs.length - 1];
-        }
+        handleOnResults(results);
       }
-  
-      handleOnResults(results);
+
       setIsFetching(false);
       return unsubscribe;
     }
     getCollection();
-  }, [query, isEnabled, handleOnResults, page, withRealtimeUpdates]);
+  }, [getCachedData, constraints?.limit, query, isEnabled, handleOnResults, withRealtimeUpdates]);
 
   const value = useMemo(() => ({
     isFetching,
