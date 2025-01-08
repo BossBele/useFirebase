3b65bdbe3d532d94122adddf73e4d28527295c05
Adam Beleko <bossbele@larven.io>
2025-01-09 00:48:18 +0300

    add database id on firestore apis - src/admin/database/firestore/getDocumentIds.ts

--- src/admin/database/firestore/getDocumentIds.ts
+++ src/admin/database/firestore/getDocumentIds.ts
@@ -4,10 +4,11 @@ import getFirestore from "./getFirestore";
  * @function getDocumentsIds
  * Get document ids in a collection
  * @param {string} collection
+ * @param {string} [databaseId] - Optional database Id
  * @returns {Promise<string[]>}
  */
-export default async function getDocumentsIds(collection: string): Promise<string[]> {
-  const firestore = getFirestore();
+export default async function getDocumentsIds(collection: string, databaseId?: string): Promise<string[]> {
+  const firestore = getFirestore(databaseId);
   const store = firestore.collection(collection);
 
   const snapshot = await store.listDocuments();
