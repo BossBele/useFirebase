ce13e1f599d3125e3cc683056b384475efc7d14f
Adam Beleko <bossbele@larven.io>
2025-01-09 00:48:17 +0300

    add database id on firestore apis - src/admin/database/firestore/getCollectionRef.ts

--- src/admin/database/firestore/getCollectionRef.ts
+++ src/admin/database/firestore/getCollectionRef.ts
@@ -1,7 +1,7 @@
 import { CollectionReference } from "firebase-admin/firestore";
 import getFirestore from "./getFirestore";
 
-export default function getCollectionRef(collection: string): CollectionReference {
-  const firestore = getFirestore();
+export default function getCollectionRef(collection: string, databaseId?: string): CollectionReference {
+  const firestore = getFirestore(databaseId);
   return firestore.collection(collection);
 }
