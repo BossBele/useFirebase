d13d00d464da085419b7130441de31794cb86dc4
Adam Beleko <bossbele@larven.io>
2025-01-17 00:31:31 +0300

    add storage apis - src/web/functions/getFunctions.ts

--- src/web/functions/getFunctions.ts
+++ src/web/functions/getFunctions.ts
@@ -1,9 +1,9 @@
 import { getFunctions as getFunctionsFB, Functions } from 'firebase/functions';
-import { getApp } from '..';
+import { getApps } from '..';
 import type { FirebaseApp } from 'firebase/app';
 
 export default function getFunctions(app?: FirebaseApp): Functions|null {
-  if (getApp()) {
+  if (getApps()?.length) {
     const functions = getFunctionsFB(app);
     return functions;
   }
