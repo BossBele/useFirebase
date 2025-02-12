5eaa5d449165d065fabc9d766dd062a4f2d4e8a9
Adam Beleko <bossbele@larven.io>
2025-02-12 16:50:25 +0300

    update upload function

--- src/web/storage/uploadFiles.ts
+++ src/web/storage/uploadFiles.ts
@@ -1,6 +1,5 @@
-import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
-import getStorage from './getStorage';
 import type { StorageFile, UploadResult } from './types';
+import uploadFile from './uploadFile';
 
 /**
  * @function uploadFiles
@@ -11,65 +10,10 @@ import type { StorageFile, UploadResult } from './types';
  */
 export default async function uploadFiles(files: StorageFile[], pathname: string, shouldSeparatePath?: boolean): Promise<{ files: string[], filePaths: string[] }|UploadResult[]> {
   if (!files?.length) {
-    throw new Error('No image/file passed');
+    throw new Error('No images/files passed');
   }
 
-  const storage = getStorage();
-  const uploadTasks:Promise<UploadResult>[] = files.map(async (file: StorageFile) => {
-    return new Promise((resolve, reject) => {
-      const extensions = pathname.split('.');
-      const extension = extensions[extensions.length - 1];
-      let hasExtension = false;
-      if (typeof extension === 'string' && extension?.length <= 4) {
-        hasExtension = true;
-      }
-      const filePath = hasExtension ? pathname : `${pathname}/${(file as File)?.name ?? Math.random().toString(36).substring(2)}`;
-      const storageRef = ref(storage, filePath);
-
-      const uploadTask = uploadBytesResumable(storageRef, file);
-
-      uploadTask.on(
-        'state_changed',
-        (snapshot) => {
-          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log(`Upload is ${progress}% done`);
-          switch (snapshot.state) {
-            case 'paused':
-              console.log('Upload is paused');
-              break;
-            case 'running':
-              console.log('Upload is running');
-              break;
-            case 'canceled':
-              console.log('Upload is canceled');
-              break;
-            case 'success':
-              console.log('Upload is completed');
-              break;
-            case 'error':
-              console.log('Upload is completed');
-              break;
-            default:
-              console.log('Upload is running');
-              break;
-          }
-        },
-        (error) => {
-          console.error('Upload failed:', error);
-          reject(error);
-        },
-        async () => {
-          try {
-            const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
-            resolve({ downloadURL, filePath } as UploadResult);
-          } catch (error) {
-            reject(error);
-          }
-        }
-      );
-    });
-  });
-
+  const uploadTasks:Promise<UploadResult>[] = files.map((file: StorageFile) => uploadFile(file, pathname));
   const results = await Promise.all(uploadTasks);
 
   if (shouldSeparatePath) {
